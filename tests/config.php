4e1de13047758b01a7266146b3f38fb5f4a0373b
Bole Chen <avenger@php.net>
2019-12-14 19:16:10 +0800

    fix doc.

--- tests/config.php
+++ tests/config.php
@@ -10,17 +10,17 @@
 
 // Config for test only
 return [
-    'apiKey'      => '58ead180d70a49048c8df010124fb9d7',
-    'apiSecret'   => '9f4cd5294355e37a75168036eb82c4db',
+    'apiKey' => '58ead180d70a49048c8df010124fb9d7',
+    'apiSecret' => '9f4cd5294355e37a75168036eb82c4db',
     'contractUrl' => 'https://contract-qa.gongmall.com/url_contract.html?companyId=qP3gZP&positionId=4Pmp2V',
 
-    'debug'   => true, // 输出日志
+    'debug' => true, // 输出日志
     'sandbox' => true, //沙盒
 
     'log' => [
-        'name'       => 'gongmall',
-        'file'       => __DIR__.'/gongmall.log',
-        'level'      => 'debug',
+        'name' => 'gongmall',
+        'file' => __DIR__.'/gongmall.log',
+        'level' => 'debug',
         'permission' => 0777,
     ],
 ];
